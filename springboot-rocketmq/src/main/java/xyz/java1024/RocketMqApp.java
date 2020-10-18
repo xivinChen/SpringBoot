9f4b41dfe555c70fcb2d6c9f0c219d006e48bd32
xivinChen <1250402127@qq.com>
2020-10-18 10:28:56 +0800

    v1.0

--- springboot-rocketmq/src/main/java/xyz/java1024/RocketMqApp.java
+++ springboot-rocketmq/src/main/java/xyz/java1024/RocketMqApp.java
@@ -1,9 +1,7 @@
 package xyz.java1024;
 
-import org.apache.rocketmq.common.message.Message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,11 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
-public class App 
+public class RocketMqApp
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class);
+        SpringApplication.run(RocketMqApp.class);
     }
 
 }
